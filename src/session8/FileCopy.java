2583926c05ec880c915ec8292410abb9de2cf8a4
Sudhir.Nayak <Sudhir.Nayak@WLW7-SUDHIRN.INDECOMM.LOCAL>
2017-07-31 00:33:19 +0530

    Session8 Assignment 4

---
+++ src/session8/FileCopy.java
@@ -0,0 +1,42 @@
+package session8;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+
+	public static void main(String[] args) {
+		FileInputStream in_stream = null;
+		FileOutputStream out_stream = null;
+	 
+		//writing the logic inside try catch block to handle exception
+	    	try{
+	    	    File infile =new File("D:\\Acadgild\\MyInput.txt");
+	    	    File outfile =new File("D:\\Acadgild\\MyOutput.txt");
+	 
+	    	    in_stream = new FileInputStream(infile);
+	    	    out_stream = new FileOutputStream(outfile);
+	 
+	    	    byte[] buffer = new byte[1024];
+	 
+	    	    int length;
+	    	   //Copying the contents from input file to output file
+	    	    while ((length = in_stream.read(buffer)) > 0){
+	    	    	out_stream.write(buffer, 0, length);
+	    	    }
+
+	    	    //Closing the input/output file streams
+	    	    in_stream.close();
+	    	    out_stream.close();
+
+	    	    System.out.println("File copied successfully!!");
+	 
+	    	}catch(IOException ioException){
+	    		System.out.println(ioException);
+	    	 }
+
+	}
+
+}
